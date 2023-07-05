269147c45206d98a46feb4a539bd8dbf80cc25d9
fasilpaloli <palolifasil@gmail.com>
2023-07-05 14:15:54 +0530

    updates

--- README.md
+++ README.md
@@ -17,3 +17,42 @@ You can install code-factory-osperb globally using npm:
 
 ```bash
 npm install -g code-factory-osperb
+
+```
+
+Usage
+To generate the CRUD scripts and associated files for a model, simply run the following command:
+
+```bash
+code-factory-osperb <model-name>
+```
+
+Replace <model-name> with the name of your model. 
+
+eg:  
+```bash
+code-factory-osperb User
+```
+
+This will generate the necessary files in the appropriate directories of your project.
+
+Template Customization
+code-factory-osperb allows you to customize the templates used for generating the files. By default, it includes the following templates:
+
+controllerTemplate.ejs
+dtoTemplate.ejs
+interfaceTemplate.ejs
+routeTemplate.ejs
+serviceTemplate.ejs
+
+You can modify these templates or create your own templates based on your requirements. Make sure to keep the placeholders <%= modelName %> and <% fields.forEach(...) %> intact as they are used for dynamic content insertion.
+
+License
+This project is licensed under the MIT License.
+
+Issues and Contributions
+If you encounter any issues or have suggestions for improvements, please open an issue on the GitHub repository.
+
+Pull requests are welcome! If you would like to contribute to this project, feel free to submit a pull request.
+
+for more: www.osperb.com
--- generate.js
+++ generate.js
@@ -1,22 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const ejs_1 = require("ejs");
-const ts_morph_1 = require("ts-morph");
+const fs = require("fs");
+const ejs = require("ejs");
+const ts_morph = require("ts-morph");
 const path = require("path");
 
 function ensureDirectoryExistence(filePath) {
   const dirname = path.dirname(filePath);
-  if (fs_1.existsSync(dirname)) {
+  if (fs.existsSync(dirname)) {
     return true;
   }
   ensureDirectoryExistence(dirname);
-  fs_1.mkdirSync(dirname);
+  try {
+    fs.mkdirSync(dirname);
+  } catch (error) {
+    if (error.code !== "EEXIST") {
+      throw error;
+    }
+  }
 }
 
 function generateFiles(modelName) {
   try {
-    const project = new ts_morph_1.Project();
+    const project = new ts_morph.Project();
     let sourceFile;
     try {
       sourceFile = project.addSourceFileAtPath(
@@ -29,21 +35,21 @@ function generateFiles(modelName) {
     const fields = [];
     const schemaDeclaration = sourceFile.getVariableDeclarationOrThrow(`${modelName.toLowerCase()}Schema`);
     const schemaInitializer = schemaDeclaration.getInitializerIfKindOrThrow(
-      ts_morph_1.SyntaxKind.NewExpression
+      ts_morph.SyntaxKind.NewExpression
     );
     const schemaObjectLiteral = schemaInitializer.getArguments()[0].asKindOrThrow(
-      ts_morph_1.SyntaxKind.ObjectLiteralExpression
+      ts_morph.SyntaxKind.ObjectLiteralExpression
     );
 
     schemaObjectLiteral.forEachDescendant((descendant) => {
-      if (descendant.getKind() === ts_morph_1.SyntaxKind.PropertyAssignment) {
+      if (descendant.getKind() === ts_morph.SyntaxKind.PropertyAssignment) {
         const assignment = descendant;
         const initializer = assignment.getInitializerIfKind(
-          ts_morph_1.SyntaxKind.ObjectLiteralExpression
+          ts_morph.SyntaxKind.ObjectLiteralExpression
         );
         if (initializer) {
           const typeProperty = initializer.getProperty("type");
-          if (typeProperty && typeProperty.getKind() === ts_morph_1.SyntaxKind.PropertyAssignment) {
+          if (typeProperty && typeProperty.getKind() === ts_morph.SyntaxKind.PropertyAssignment) {
             const typeAssignment = typeProperty;
             const typeName = typeAssignment.getInitializer()?.getText();
             if (typeName) {
@@ -57,61 +63,50 @@ function generateFiles(modelName) {
     const templateData = { modelName: modelName, fields: fields };
     const templatesFolderPath = path.join(__dirname, "templates");
 
-    const serviceTemplate = fs_1.readFileSync(
+    const serviceTemplate = fs.readFileSync(
       path.join(templatesFolderPath, "serviceTemplate.ejs"),
       "utf-8"
     );
-    const controllerTemplate = fs_1.readFileSync(
+    const controllerTemplate = fs.readFileSync(
       path.join(templatesFolderPath, "controllerTemplate.ejs"),
       "utf-8"
     );
-    const interfaceTemplate = fs_1.readFileSync(
+    const interfaceTemplate = fs.readFileSync(
       path.join(templatesFolderPath, "interfaceTemplate.ejs"),
       "utf-8"
     );
-    const dtoTemplate = fs_1.readFileSync(
+    const dtoTemplate = fs.readFileSync(
       path.join(templatesFolderPath, "dtoTemplate.ejs"),
       "utf-8"
     );
-    const routeTemplate = fs_1.readFileSync(
+    const routeTemplate = fs.readFileSync(
       path.join(templatesFolderPath, "routeTemplate.ejs"),
       "utf-8"
     );
 
-    const serviceContent = ejs_1.render(serviceTemplate, templateData);
-    const controllerContent = ejs_1.render(controllerTemplate, templateData);
-    const interfaceContent = ejs_1.render(interfaceTemplate, templateData);
-    const dtoContent = ejs_1.render(dtoTemplate, templateData);
-    const routeContent = ejs_1.render(routeTemplate, templateData);
+    const serviceContent = ejs.render(serviceTemplate, templateData);
+    const controllerContent = ejs.render(controllerTemplate, templateData);
+    const interfaceContent = ejs.render(interfaceTemplate, templateData);
+    const dtoContent = ejs.render(dtoTemplate, templateData);
+    const routeContent = ejs.render(routeTemplate, templateData);
 
-    const outputFolderPath = path.join(__dirname, "src");
+    var servicePath = "./src/services/".concat(modelName.toLowerCase(), ".service.ts");
+    var controllerPath = "./src/controllers/".concat(modelName.toLowerCase(), ".controller.ts");
+    var interfacePath = "./src/interfaces/".concat(modelName.toLowerCase(), ".interface.ts");
+    var dtoPath = "./src/dtos/".concat(modelName.toLowerCase(), ".dto.ts");
+    var routePath = "./src/routes/".concat(modelName.toLowerCase(), ".route.ts");
 
-    ensureDirectoryExistence(path.join(outputFolderPath, "services"));
-    ensureDirectoryExistence(path.join(outputFolderPath, "controllers"));
-    ensureDirectoryExistence(path.join(outputFolderPath, "interfaces"));
-    ensureDirectoryExistence(path.join(outputFolderPath, "dtos"));
-    ensureDirectoryExistence(path.join(outputFolderPath, "routes"));
+    ensureDirectoryExistence(servicePath);
+    ensureDirectoryExistence(controllerPath);
+    ensureDirectoryExistence(interfacePath);
+    ensureDirectoryExistence(dtoPath);
+    ensureDirectoryExistence(routePath);
 
-    fs_1.writeFileSync(
-      path.join(outputFolderPath, "services", `${modelName.toLowerCase()}.service.ts`),
-      serviceContent
-    );
-    fs_1.writeFileSync(
-      path.join(outputFolderPath, "controllers", `${modelName.toLowerCase()}.controller.ts`),
-      controllerContent
-    );
-    fs_1.writeFileSync(
-      path.join(outputFolderPath, "interfaces", `${modelName.toLowerCase()}.interface.ts`),
-      interfaceContent
-    );
-    fs_1.writeFileSync(
-      path.join(outputFolderPath, "dtos", `${modelName.toLowerCase()}.dto.ts`),
-      dtoContent
-    );
-    fs_1.writeFileSync(
-      path.join(outputFolderPath, "routes", `${modelName.toLowerCase()}.route.ts`),
-      routeContent
-    );
+    fs.writeFileSync(servicePath, serviceContent);
+    fs.writeFileSync(controllerPath, controllerContent);
+    fs.writeFileSync(interfacePath, interfaceContent);
+    fs.writeFileSync(dtoPath, dtoContent);
+    fs.writeFileSync(routePath, routeContent);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
